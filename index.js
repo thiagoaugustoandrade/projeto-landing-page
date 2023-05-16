7d7053ab9d4d6b916631a2ee79f1b24623a981d1
Thiago Andrade <132917483+thiagoaugustoandrade@users.noreply.github.com>
2023-05-16 13:19:39 -0300

    projeto Landing Page concluido

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var leonardo =
+var setaesquerda = document.getElementById("seta-esquerda")
+var leonardo = document.getElementById("leonardo")
+var samanta = document.getElementById("samanta")
+var bruna = document.getElementById("bruna")
+var setadireita = document.getElementById("seta-direita")
+
+function rolardireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style = "display:flex; margin-left: 32px; margin-top: 116px"
+   
+    
+}
+
+function rolaresquerda(){
+    bruna.style = "display: none"
+    leonardo.style = "display: flex"
+    setadireita.style = "display: flex; margin-left: 32px; margin-top: 116px"
+    setaesquerda.style = "display: none"
+}
